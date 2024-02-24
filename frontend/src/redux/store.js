b4e5c807503ce343627b9f0eb0ac29479415fae6
SHIVAM KASERA <skshivamkasera2003@gmail.com>
2024-02-24 22:21:22 +0530

    update

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -10,10 +10,12 @@ export const store = configureStore({
     }
 })
 
-export const FRONTEND_URL = "http://localhost:3000"
+export const FRONTEND_URL = "https://viewtube-v001.onrender.com"
+// export const FRONTEND_URL = "http://localhost:3000"
 // export const FRONTEND_URL = "http://192.168.1.3:3000"
 
-export const REQUEST_URL = "http://localhost:5000/api/v1"
+export const REQUEST_URL = "https://viewtube-v001.onrender.com/api/v1"
+// export const REQUEST_URL = "http://localhost:5000/api/v1"
 // export const REQUEST_URL = "http://192.168.1.3:5000/api/v1"
 
 export const POST_CONFIG = {
